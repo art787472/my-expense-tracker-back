c70a318b09eeff511a0aadaefb9b051f6d84d0b4
YICHENG\art78 <art787472@gmail.com>
2025-07-19 20:33:34 +0800

    新增Category、SubCategory、Icon，以及更新資料庫

---
+++ 記帳程式後端/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace 記帳程式後端.Models
+{
+    public class Category
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int IconId { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ 記帳程式後端/Models/Icon.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace 記帳程式後端.Models
+{
+    public class Icon
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
