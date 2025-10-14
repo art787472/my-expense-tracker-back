3fc07dd27702a914a504cf1abad9b5fabbf2c56d
YICHENG\art78 <art787472@gmail.com>
2025-10-14 16:07:44 +0800

    重新修改deploy.yml

---
+++ ExpenseTracker.Test/ExpenseController/ExpenseControllerTest.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExpenseTracker.Controllers;
+using ExpenseTracker.Dto;
+using ExpenseTracker.Repository;
+using ExpenseTracker.Service;
+using FakeItEasy;
+using FluentAssertions;
+
+namespace ExpenseTracker.Test.ExpenseControllerTest
+{
+    public class ExpenseControllerTest
+    {
+        
+        private readonly IExpenseService _expenseService;
+        private readonly ICurrentUserService _currentUser;
+
+        public ExpenseControllerTest() 
+        {
+            _expenseService = A.Fake<IExpenseService>();
+            _currentUser = A.Fake<ICurrentUserService>();
+        }
+
+        [Fact]
+        public async void ExpenseController_GetExpenseById()
+        {
+            var expense = A.Fake<ExpenseDto>();
+            var controller = new ExpenseController(_expenseService, _currentUser);
+            int expenseId = 1;
+            var fakeExpense = new ExpenseDto
+            {
+                Id = expenseId,
+                Name = "Test Expense",
+                price = 100
+            };
+
+            A.CallTo(() => _expenseService.GetExpenseById(expenseId))
+                .Returns(Task.FromResult(fakeExpense));
+            var result = await controller.GetExpenseById(expenseId);
+
+            result.Should().NotBeNull();
+        }
+    }
+}
